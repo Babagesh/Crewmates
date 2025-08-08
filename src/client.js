a82f9dd557056a9db858b78403531f4378fdda08
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2025-08-08 13:56:32 -0400

    secure api keys and url

--- src/client.js
+++ src/client.js
@@ -1,7 +1,5 @@
 import {createClient} from '@supabase/supabase-js'
-
-const URL = 'https://zuyreadklzzbfdqqgyvd.supabase.co';
-
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1eXJlYWRrbHp6YmZkcXFneXZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4NDU4NTIsImV4cCI6MjA2OTQyMTg1Mn0.n6DpLY8CGLJw62Ut6JNrDEA58v7V5xQzfnEQj6zblzE';
+const API_KEY = import.meta.env.VITE_CREWMATE_URL;
+const URL = import.meta.env.VITE_CREWMATE_API_KEY;
 
 export const supabase = createClient(URL, API_KEY);
